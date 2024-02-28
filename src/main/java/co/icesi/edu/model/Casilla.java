850008542c07502c0eb8d46174360cce8c8d6c77
LunaKtalina <lunamartinez5619@gmail.com>
2024-02-28 09:29:16 -0500

    clases 1

---
+++ src/main/java/co/icesi/edu/model/Casilla.java
@@ -0,0 +1,8 @@
+package co.icesi.edu.model;
+
+public class Casilla {
+
+    private Jugador jugador;
+    private Casilla nextCasilla;
+
+}
---
+++ src/main/java/co/icesi/edu/model/Jugador.java
@@ -0,0 +1,11 @@
+package co.icesi.edu.model;
+
+public class Jugador {
+    private int posicion;
+    private String nombre;
+
+    public Jugador(String n, int p) {
+        this.nombre = n;
+        this.posicion = p;
+    }
+}
